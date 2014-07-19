fef133c0afcec729e3964eafa6743b4517a6043b
igorrosenberg <igor.rosenberg@gmail.com>
2014-07-19 22:45:38 +0200

    Config command object

--- grails-app/controllers/grails/plugin/touml/UmlController.groovy
+++ grails-app/controllers/grails/plugin/touml/UmlController.groovy
@@ -2,10 +2,13 @@ package grails.plugin.touml
 
 class UmlController {
 
-    static defaultAction = "domain"
+    // static defaultAction = "domain"
     
     def umlService
-         
+       
+    def index(ConfigurationCommand config){      
+      render "fieldFilterRegexps=${config.fieldFilterRegexps} classFilterRegexps=${config.classFilterRegexps} duplicateFieldAndArrow=${config.duplicateFieldAndArrow} diagramType=${config.diagramType}"
+    }
     def domain() {
         def umlURL = umlService.domain()
         redirect url:umlURL
@@ -17,3 +20,21 @@ class UmlController {
     }
     
 }
+
+/**
+* Command object for Configuration options
+*/
+class ConfigurationCommand { 
+  /** Filters restricting the visibility of fields within classes */
+  String [] fieldFilterRegexps
+  /** Filters restricting the visibility of classes */
+  String [] classFilterRegexps
+  /** Should properties which generate an arrow be listed in the origin class */
+  boolean duplicateFieldAndArrow
+  DiagramType diagramType 
+  }
+  
+enum DiagramType {
+    DOMAIN, LAYERS
+}
+  
