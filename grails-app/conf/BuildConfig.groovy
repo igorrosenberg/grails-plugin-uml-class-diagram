46a56963a82cf58d84136a60635a55cdbe110c07
igor.rosenberg <igor.rosenberg@gmail.com>
2014-12-12 22:53:08 +0100

    grails 2.2 style

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,8 +16,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
-            export = false
-        }
-    }
+			build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			   export = false
+			}
 }
