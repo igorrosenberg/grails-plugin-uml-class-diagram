ffc373b533215d43dcdb887d6c16386c48a2eb32
igor.rosenberg <igor.rosenberg@gmail.com>
2014-12-12 23:00:59 +0100

    user guide & github urls

--- UmlClassDiagramGrailsPlugin.groovy
+++ UmlClassDiagramGrailsPlugin.groovy
@@ -4,8 +4,8 @@ class UmlClassDiagramGrailsPlugin {
     def title = "UML Class Diagram Plugin"
     def author = "Igor Rosenberg"
     def description = 'Generate UML class diagrams from your Grails app source code.'
-    def documentation = "https://github.com/igorrosenberg/grails-plugin-uml-class-diagram/"
+    def documentation = "http://igorrosenberg.github.io/grails-plugin-uml-class-diagram/"
     def license = "APACHE"
-    def issueManagement = [ system: "JIRA", url: "https://github.com/igorrosenberg/grails-plugin-uml-class-diagram/issues" ]
+    def issueManagement = [ system: 'github', url: "https://github.com/igorrosenberg/grails-plugin-uml-class-diagram/issues" ]
     def scm = [ url: "https://github.com/igorrosenberg/grails-plugin-uml-class-diagram/" ]
 }
