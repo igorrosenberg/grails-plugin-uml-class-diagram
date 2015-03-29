adcae99a11252985b46b8298530638694d73cec8
igor.rosenberg <igor.rosenberg@gmail.com>
2015-03-29 20:46:51 +0200

    Reverting release-plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,6 +16,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-			build ':release:3.1.0'
+			build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			   export = false
+			}
 	}
 }
