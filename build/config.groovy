c6f247d1425458fe05ea8c73102c3e1f260ac2dc
Igor Rosenberg <igor.rosenberg3@docapost.fr>
2017-03-01 13:43:39 +0100

    rm_build

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.7')
-        classNode.putNodeMetaData('projectName', 'uml-class-diagram')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
